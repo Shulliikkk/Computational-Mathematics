dc998d049f82ee3e5e95b377aa9c1d5ea021210e
Shulgin Konstantin <68589028+Shulliikkk@users.noreply.github.com>
2024-09-22 04:04:48 +0300

    Update structure

--- compDiff/numDiff.cpp
+++ compDiff/numDiff.cpp
@@ -1,101 +1,165 @@
-#include <iostream>
 #include <array>
+#include <stdexcept>
+#include <utility>
 #include <cmath>
+#include <iostream>
 
-template<typename RealType>
-RealType fact(RealType L) {
-  RealType fact = 1;
-  for (unsigned int i = 2; i < L + 1; i++) {
-    fact *= i;
-  }
-  return fact;
-}
-
-template<typename RealType, unsigned int N>
-struct DerivativeCoef {
-    RealType centralCoef;
-    std::array<RealType, N> otherCoefs;
+template<typename T, std::size_t N>
+struct System {
+  std::array<std::array<T, N + 1>, N + 1> matrix{};
+  std::array<T, N + 1> col{};
 };
 
-template<typename RealType, unsigned int N, unsigned int L>
-DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N>& points) noexcept {
-  // создание и заполнения матрицы СЛАУ для метода неопределенных коэффициентов
-  unsigned int i_0;
+template<typename T, unsigned int N>
+struct Grid {
+  std::array<T, N> points{};
+  std::size_t central_point;
+};
 
-  if (points[0] > 0) {
-    i_0 = 0;
-  }
-  else if (points[N - 1] < 0) {
-    i_0 = N;
+template<typename T, unsigned int N>
+struct Derivative_coef {
+    T central_coef;
+    std::array<T, N> other_coefs{};
+};
+
+template<std::size_t... Ints>
+constexpr std::size_t mult(std::index_sequence<Ints...>) noexcept {
+  return ((Ints + 1) * ...);
+}
+
+template<std::size_t I>
+constexpr std::size_t fact() noexcept {
+  if constexpr (I == 0) {
+    return 1;
   }
   else {
-    for (unsigned int i = 0; i < N - 1; i++) {
-      if (points[i] < 0 && points[i + 1] > 0) { // points[i] + 1 == 0
-        i_0 = i + 1;
-        break;
-      }
-    }
+    return mult(std::make_index_sequence<I>());
   }
+}
 
-  std::array<std::array<RealType, N + 2>, N + 1> A = {};
-  for (unsigned int i = 0; i < N + 1; i++) {
-		for (unsigned int j = 0;  j < N + 1; j++) {
-      if (i < i_0) {
-        A[j][i] = std::pow(points[i], j);
-      }
-      else if (i == i_0) {
-        A[j][i] = std::pow(0, j);
-      }
-      else {
-        A[j][i] = std::pow(points[i - 1], j);
-      }
+template<typename T, std::size_t N>
+std::array<std::array<T, N - 1>, N - 1> minor(const std::array<std::array<T, N>, N>& matrix, const std::size_t i, const std::size_t j) noexcept {
+    std::array<std::array<T, N - 1>, N - 1> minor_res;
+    std::size_t row, col;
+    for (std::size_t k = 0; k < N; k++) {
+        for (std::size_t m = 0; m < N; m++) {
+            if (k == i || m == j) {
+                continue;
+            }
+            row = (k > i) ? k - 1 : k;
+            col = (m > j) ? m - 1 : m;
+            minor_res[row][col] = matrix[k][m];
+        }
     }
-  }
-  for (unsigned int j = 0; j < N + 2; j++) {
-    A[L][N + 1] = fact(L);
-  }
+    return minor_res;
+}
 
-  // решение СЛАУ методом Гаусса
-  for (unsigned int i = 0; i < N + 1; i++) { // выбор ведущей строки
-    float A_ii = A[i][i];
-    for (unsigned int j = 0; j < N + 2; j++) { // нормировка ведущей строки
-      A[i][j] /= A_ii;
+template<typename T, std::size_t N>
+T determinant(const std::array<std::array<T, N>, N>& matrix) noexcept {
+    T det_res = 0;
+    const std::size_t i = 0;
+    if constexpr (N == 1) {
+        return matrix[0][0];
     }
-    for (unsigned int n = 0; n < N + 1; n++) { // проход по строкам
-      if (n != i) {
-        int A_ni = A[n][i];
-        for (unsigned int k = 0; k < N + 2; k++) { // проход по выбранной строке
-          A[n][k] -= A_ni * A[i][k];
+    else {
+        for (std::size_t j = 0; j < N; j++) {
+            det_res += ((i + j) % 2 == 0 ? 1 : -1) * matrix[i][j] * determinant<T, N - 1>(minor<T, N>(matrix, i, j));
+        }
+    }
+    return det_res;
+}
+
+template<typename T, size_t N>
+T det_change_col(std::array<std::array<T, N>, N> matrix, const std::array<T, N>& col, const std::size_t j) noexcept {
+    for (std::size_t i = 0; i < N; i++) {
+        matrix[i][j] = col[i];
+    }
+    return determinant<T, N>(matrix);
+}
+
+template<typename T, std::size_t N, std::size_t L>
+System<T, N> create_system(Grid<T, N>& nodes) noexcept {
+    static_assert(N > L, "N must be greater than L");
+    System<T, N> system;
+    if (nodes.points[0] > 0) {
+      nodes.central_point = 0;
+    }
+    else if (nodes.points[N - 1] < 0) {
+      nodes.central_point = N;
+    }
+    else {
+      for (std::size_t i = 0; i < N - 1; i++) {
+        if (nodes.points[i] < 0 && nodes.points[i + 1] > 0) {
+          nodes.central_point = i + 1;
+          break;
         }
       }
     }
-  }
-
-  // формирование ответа
-  DerivativeCoef<RealType, N> derivativeCoef;
-  for (unsigned int i = 0; i < N + 1; i++) {
-    if (i < i_0) {
-      derivativeCoef.otherCoefs[i] = A[i][N + 1];
-
+    for (std::size_t i = 0; i < N + 1; i++) {
+  		for (std::size_t j = 0;  j < N + 1; j++) {
+        if (i < nodes.central_point) {
+          system.matrix[j][i] = std::pow(nodes.points[i], j);
+        }
+        else if (i == nodes.central_point) {
+          system.matrix[j][i] = (j == 0) ? 1 : 0;
+        }
+        else {
+          system.matrix[j][i] = std::pow(nodes.points[i - 1], j);
+        }
+      }
     }
-    else if (i == i_0) {
-      derivativeCoef.centralCoef = A[i][N + 1];
-    }
-    else {
-      derivativeCoef.otherCoefs[i - 1] = A[i][N + 1];
-    }
-  }
-  return derivativeCoef;
+    system.col[L] = fact<L>();
+    return system;
 }
 
-int main() {
-  const unsigned int N = 3, L = 2;
-  std::array<float, N> points = {1, 2, 3};
-  DerivativeCoef<float, N> derivativeCoef = calcDerivativeCoef<float, N, L>(points);
-  std::cout << "centralCoef = " << derivativeCoef.centralCoef << std::endl;
-  std::cout << "otherCoefs = {";
-  for (unsigned int i = 0; i < N - 1; i++) {
-      std::cout << derivativeCoef.otherCoefs[i] << ", ";
+template<typename T, std::size_t N>
+std::array<T, N> solve(const System<T, N>& system) {
+    std::array<T, N> res;
+    const T det = determinant<T, N + 1>(system.matrix);
+    if (det == 0) {
+        throw std::runtime_error("Determinant is zero");
+    }
+    for (std::size_t i = 0; i < N + 1; ++i) {
+        res[i] = det_change_col<T, N + 1>(system.matrix, system.col, i) / det;
+    }
+    return res;
+}
+
+template<typename T, std::size_t N, std::size_t L>
+Derivative_coef<T, N> calc_derivative_coef(Grid<T, N>& nodes) {
+  Derivative_coef<T, N> derivative_coef;
+  System<T, N> system = create_system<T, N, L>(nodes);
+  std::array<T, N> solution = solve<T, N>(system);
+  derivative_coef.central_coef = solution[nodes.central_point];
+  for (unsigned int i = 0; i < N + 1; i++) {
+    if (i < nodes.central_point) {
+      derivative_coef.other_coefs[i] = solution[i];
+    }
+    else if (i == nodes.central_point) {
+      derivative_coef.central_coef = solution[i];
+    }
+    else {
+      derivative_coef.other_coefs[i - 1] = solution[i];
+    }
   }
-  std::cout << derivativeCoef.otherCoefs[N - 1] << '}';
+  return derivative_coef;
+}
+
+int main(){
+  try {
+    const std::size_t N = 3, L = 2;
+    Grid<double, N> nodes;
+    nodes.points = std::array<double, N>{1, 2, 3};
+    Derivative_coef<double, N> derivative_coef = calc_derivative_coef<double, N, L>(nodes);
+    std::cout << "central_coef = " << derivative_coef.central_coef << std::endl;
+    std::cout << "other_coefs = {";
+    for (std::size_t i = 0; i < N - 1; i++) {
+        std::cout << derivative_coef.other_coefs[i] << ", ";
+    }
+    std::cout << derivative_coef.other_coefs[N - 1] << '}';
+  }
+  catch (const std::runtime_error& e) {
+
+    std::cerr << "Eror: " << e.what() << std::endl;
+  }
 }
