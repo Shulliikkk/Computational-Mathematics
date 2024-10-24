58258beb30503e4515a86ddbda81d02f2b6af0fd
Shulgin Konstantin <68589028+Shulliikkk@users.noreply.github.com>
2024-10-24 17:39:52 +0300

    Fixed nodes and weights

--- Gauss_Quadrature/Gauss.cpp
+++ Gauss_Quadrature/Gauss.cpp
@@ -11,10 +11,10 @@ template<typename T, std::size_t N>
 T elem_gauss_quad(const std::function<T(T)> func, const T x_prev, const T x_next) {
     constexpr std::array<T, N> nodes = []{
         if constexpr (N == 5) {
-            return std::array<T, 5>{-0.906113631, -0.53846931, 0, 0.53846931, 0.906113631};
+            return std::array<T, 5>{-0.906179845938664, -0.538469310105683, 0, 0.538469310105683, 0.906179845938664};
         }
         else if constexpr (N == 8) {
-            return std::array<T, 8>{-0.96028986, -0.79666648, -0.52553242, -0.18343464, 0.18343464, 0.52553242, 0.79666648, 0.96028986};
+            return std::array<T, 8>{-0.9602898564975362, -0.7966664774136267, -0.5255324099163289, -0.1834346424956498, 0.1834346424956498, 0.5255324099163289, 0.7966664774136267, 0.9602898564975362};
         }
         else {
             static_assert(N == 5 || N == 8, "N must be either 5 or 8");
@@ -64,13 +64,13 @@ constexpr std::size_t deg_2() {
 }
 
 template<typename T, std::size_t N>
-T runge_gauss_quad(const std::function<T(T)> func, const T start, const T end, const T err) {
+std::pair<T, T> runge_gauss_quad(const std::function<T(T)> func, const T start, const T end, const T err) {
     std::size_t M = 10;
     T I_h, I_h2, Delta_I, I;
     I_h = gauss_quad<T, N>(func, start, end, M);
     while (true) {
         I_h2 = gauss_quad<T, N>(func, start, end, 2 * M);
-        Delta_I = (I_h - I_h2) / (deg_2<N + 1>() - 1);
+        Delta_I = (I_h - I_h2) / (deg_2<N>() - 1);
         if (Delta_I < err || M > 100000) {
             I = I_h2 - Delta_I;
             break;
@@ -78,16 +78,39 @@ T runge_gauss_quad(const std::function<T(T)> func, const T start, const T end, c
         M *= 2;
         I_h = I_h2;
     }
-    return I;
+    return {I, (end - start) / M};
 }
 
 int main() {
-    double err, eps = 1.0;
-    double I, I_accur = 1.839071529076452452258864;
-    for (std::size_t i = 0; i < 15; i++) {
-        I = runge_gauss_quad<double, 5>(f<double>, 0, 10, eps);
+    const std::size_t N = 8;
+    std::array<double, N> H;
+    std::size_t M = 1;
+    const double start = 0, end = 10;
+    double err, eps = 0.000001;
+    double I, I_accur = 1.8390715290764524;
+
+    /*
+    for (std::size_t i = 0; i < N; i++) {
+        std::pair<double, double> rgq = runge_gauss_quad<double, 5>(f<double>, 0, 10, eps);
+        I = rgq.first;
+        H[i] = rgq.second;
+        //auto [I, h] = runge_gauss_quad<double, 5>(f<double>, 0, 10, eps);
         err = std::abs(I - I_accur);
-        std::cout << err << ' ';
+        std::cout << err << ", ";
         eps *= 0.1;
+    }
+    */
+
+    for (std::size_t i = 0; i < N; i++) {
+        double I = gauss_quad<double, 5>(f<double>, start, end, M);
+        H[i] = (end - start) / M;
+        err = std::abs(I - I_accur);
+        std::cout << err << ", ";
+        M *= 2;
+    }
+
+    std::cout << '\n';
+    for (std::size_t i = 0; i < N; i++) {
+      std::cout << H[i] << ", ";
     }
 }
