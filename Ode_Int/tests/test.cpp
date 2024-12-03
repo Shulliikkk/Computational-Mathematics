8a2fe81bfd53d72fc9f88e6ef8c8a47e72873c68
Shulgin Konstantin <68589028+Shulliikkk@users.noreply.github.com>
2024-12-03 22:05:40 +0300

    Added time saving

--- Ode_Int/tests/test.cpp
+++ Ode_Int/tests/test.cpp
@@ -4,12 +4,13 @@
 #include <../src/odeint.hpp>
 
 std::array<double, 2> f(double t, std::array<double, 2> x) {
-  std::array<double, 2> rhs{x[1], -x[0]};
+  std::array<double, 2> rhs{x[1], - x[0]};
   return rhs;
 }
 
 int main() {
   std::array<double, 2> init_condition{0.0, 1.0};
-  std::vector<std::array<double, 2>> sol = odeint<RKF45, 2>(f, init_condition, 5., 0.00001, 1e-12);
-  std::cout << sol[sol.size() - 1][0] << std::endl;
+  std::vector<Point<std::array<double, 2>>> sol = odeint<2>(f, init_condition, 5., 0.0001, 1e-12);
+  std::cout << sol[sol.size() - 1].state[0] << std::endl;
+  std::cout << sol.size();
 }
